90a9cc0ba49ddfde6619068d1a784a7d08351604
dani <dani.xtendoo@gmail.com>
2022-06-27 12:32:59 +0200

    [ADD] fha_administration

---
+++ fha_administration/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'FHA Administration',
+    'summary': """Administration settings for FHA""",
+    'version': '13.0.1.0.0',
+    'description': """Administration settings for FHA""",
+    'author': 'Dani Domínguez',
+    'company': 'Xtendoo',
+    'website': 'http://xtendoo.es',
+    'category': 'Admin Tools',
+    'depends': [
+        'base',
+        'sale',
+    ],
+    'license': 'AGPL-3',
+    'data': [
+    ],
+    'installable': True,
+    'auto_install': True,
+}
