57489ebf0971f9a4424882b818daedfeb74a83dc
dani <dani.xtendoo@gmail.com>
2024-03-22 14:47:33 +0100

    [MIG] 14 fha_dms_hr_employee

---
+++ fha_dms_hr_employee/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "FHA DMS Employee",
+    "category": "Employee",
+    "version": "14.0.1.0",
+    "author": "Xtendoo",
+    "depends": [
+        "hr",
+        "dms",
+        "dms_field",
+    ],
+    "description": """
+        Create DMS page  Employee.
+        """,
+    "data": [
+        "views/hr_employee_form_view.xml",],
+    "installable": True,
+    "auto_install": True,
+}
