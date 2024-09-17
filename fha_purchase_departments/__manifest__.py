4f1e952a12f0bce78dbd71bfbcd07b313f137d40
dani <dani.xtendoo@gmail.com>
2024-09-17 13:43:52 +0200

    [ADD] fha_purchase_departments

---
+++ fha_purchase_departments/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 Xtendoo
+
+{
+    "name": "FHA Purchase Departments",
+    "summary": """
+        FHA Purchase Departments""",
+    "version": "16.0",
+    "depends": ["purchase","hr"],
+    "maintainers": ["Dani Domínguez"],
+    "author": "Xtendoo",
+    "license": "AGPL-3",
+    "data": [
+        "views/purchase_order_views.xml"
+            ],
+    "installable": True,
+    "auto_install": True,
+}
+
