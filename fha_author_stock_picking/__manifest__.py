69a30b8aa592a9a2fe2cfafed5fc5640681f25ea
dani <dani.xtendoo@gmail.com>
2024-09-17 13:03:04 +0200

    [ADD] fha_author_stock_picking

---
+++ fha_author_stock_picking/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2022 Xtendoo
+
+{
+    "name": "FHA Author Stock Picking",
+    "summary": """
+        FHA Author Stock Picking""",
+    "version": "16.0",
+    "depends": ["stock"],
+    "maintainers": ["Dani Domíngue"],
+    "author": "Dani Domínguez",
+    "license": "AGPL-3",
+    'application': True,
+    "installable": True,
+    "auto_install": True,
+}
+
