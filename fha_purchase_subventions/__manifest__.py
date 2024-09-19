e9a0cb0461fef04b6d108b88053c7ed8a211ccc5
dani <dani.xtendoo@gmail.com>
2024-09-19 10:18:14 +0200

    [ADD] fha_purchase_subventions

---
+++ fha_purchase_subventions/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2022 Xtendoo
+
+{
+    "name": "FHA Purchase Subventions",
+    "summary": """
+        FHA Purchase Subventions""",
+    "version": "16.0",
+    "depends": [
+        "purchase",
+        "fha_subvention",
+    ],
+    "maintainers": ["dariocruzmauro", "Dani Domínguez"],
+    "author": "Xtendoo",
+    "license": "AGPL-3",
+    "data": [
+        "views/purchase_order_views.xml"
+    ],
+    "installable": True,
+    "auto_install": True,
+}
+
