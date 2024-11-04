cffc4e525e7bb48db8a24c4ef6a05e42ee76da25
dani <dani.xtendoo@gmail.com>
2024-11-04 13:25:43 +0100

    [MOD] calification_in_purchase, ajuste de tier validation, para evitar que a partir de que se solicite la revision, el campo calification, pasa a ser ead only, lo cual aplica el modulo base tier validation

--- calification_in_purchase/models/purchase_order.py
+++ calification_in_purchase/models/purchase_order.py
@@ -1,7 +1,7 @@
 # Copyright 2022 Xtendoo
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class PurchaseOrder(models.Model):
@@ -13,3 +13,32 @@ class PurchaseOrder(models.Model):
         default="0",
         tracking=True,
     )
+
+BASE_EXCEPTION_FIELDS = ["message_follower_ids", "access_token", "calification"]
+
+
+class TierValidation(models.AbstractModel):
+    _inherit = "tier.validation"
+    _description = "Tier Validation (abstract)"
+
+    @api.model
+    def _get_validation_exceptions(self, extra_domain=None, add_base_exceptions=True):
+        """Return Tier Validation Exception field names that matchs custom domain."""
+        exception_fields = (
+            self.env["tier.validation.exception"]
+            .sudo()
+            .search(
+                [
+                    ("model_name", "=", self._name),
+                    ("company_id", "in", [False] + self.env.company.ids),
+                    "|",
+                    ("group_ids", "in", self.env.user.groups_id.ids),
+                    ("group_ids", "=", False),
+                    *(extra_domain or []),
+                ]
+            )
+            .mapped("field_ids.name")
+        )
+        if add_base_exceptions:
+            exception_fields += BASE_EXCEPTION_FIELDS
+        return list(set(exception_fields))
