14fbc3917803c8f2eda066a4b14a08a96224012c
dani <dani.xtendoo@gmail.com>
2024-11-15 13:47:25 +0100

    [ADD] analitic_line_in_invoice_header

---
+++ analitic_line_in_invoice_header/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Analitic line in invoice header",
+    "summary": """Analitic line in invoice header""",
+    "version": "16.0.1.0.0",
+    "description": """Analitic line in invoice header""",
+    "author": "Dani Domínguez",
+    "company": "Xtendoo",
+    "website": "http://xtendoo.es",
+    "category": "account",
+    "depends": [
+        "account",
+    ],
+    "license": "AGPL-3",
+    "data": [
+        "views/invoice_view.xml",
+    ],
+    "installable": True,
+    "auto_install": True,
+}
---
+++ analitic_line_in_invoice_header/models/account_move.py
@@ -0,0 +1,19 @@
+from odoo import api, fields, models, Command, _
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    analytic_distribution = fields.Json(string='Analytic Distribution')
+    analytic_precision = fields.Integer(
+        store=False,
+        default=lambda self: self.env['decimal.precision'].precision_get("Percentage Analytic"),
+    )
+
+    @api.onchange('invoice_line_ids')
+    def _onchange_quick_edit_line_ids(self):
+        res = super()._onchange_quick_edit_line_ids()
+        for line in self.invoice_line_ids:
+            if hasattr(line, 'analytic_distribution'):
+                line.analytic_distribution = self.analytic_distribution
+        return res
