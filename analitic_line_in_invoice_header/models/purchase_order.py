cbcecedb21b94e415e186e1c8a06f6e06ca7ed00
dani <dani.xtendoo@gmail.com>
2024-12-23 08:23:18 +0100

    sync

--- analitic_line_in_invoice_header/models/purchase_order.py
+++ analitic_line_in_invoice_header/models/purchase_order.py
@@ -4,7 +4,7 @@ from odoo import api, fields, models, Command, _
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
-    analytic_distribution = fields.Json(string='Analytic Distribution', required=True,)
+    analytic_distribution = fields.Json(string='Analytic Distribution')
     analytic_precision = fields.Integer(
         store=False,
         default=lambda self: self.env['decimal.precision'].precision_get("Percentage Analytic"),
@@ -12,8 +12,6 @@ class PurchaseOrder(models.Model):
 
     @api.onchange('order_line')
     def _onchange_quick_edit_line_ids(self):
-        # res = super()._onchange_quick_edit_line_ids()
         for line in self.order_line:
             if hasattr(line, 'analytic_distribution'):
                 line.analytic_distribution = self.analytic_distribution
-        # return res
